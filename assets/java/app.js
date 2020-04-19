ff68726702a05407dba2602ec17742a9a9237e09
Luba Pecheneva <lp5786766@gmail.com>
2020-04-18 21:30:09 -0400

    Added pseudocode and basic vars to java

---
+++ assets/java/app.js
@@ -0,0 +1,44 @@
+//create Array for each of character
+
+var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var specialArray = ["!", "@", "#", "$", "%", "^", "&", "*"];
+
+var numbersArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+
+
+//validate inputs:
+
+//create if statements to make sure number (isNaN)
+var passwordlength = prompt("How many characters?")
+// if //password too short     
+//password too longth 
+
+var hasLower = confirm("Do you want to use lowercase letters (abc...)? Yes - Ok, No - Cancel");
+
+var hasUpper = confirm("Do you want to use uppercase letters (ABC...)? Yes - Ok, No - Cancel");
+
+var hasSpecial = confirm("Do you want to use special characters (!@#$%^&*)? Yes - Ok, No - Cancel");
+
+var hasNumbers = confirm("Do you want to use numbers (123...)? Yes - Ok, No - Cancel");
+
+
+
+
+//use alert to notify user
+// alert to make sure they selected one type, if not go back to the beginning
+// if (hasUpper === false &&
+
+
+
+//return;
+
+
+
+//create a final Array of Arrays, where random items from selected arrays are stored
+//display the new password - append to the div
+
+
