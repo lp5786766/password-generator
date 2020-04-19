e78840c2a61c0c96e1010ac06349adfc0a3e9182
Luba Pecheneva <lp5786766@gmail.com>
2020-04-19 19:15:23 -0400

    Added output elements and concat to the final array

--- assets/java/app.js
+++ assets/java/app.js
@@ -1,28 +1,34 @@
 //create Array for each of character
+var lowerOutput = document.getElementById("lower-choice");
+var upperOutput = document.getElementById("upper-choice");
+var specialOutput = document.getElementById("special-choice");
+var numbersOutput = document.getElementById("numbers-choice");
 
-vars lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
 var specialArray = ["!", "@", "#", "$", "%", "^", "&", "*"];
-
 var numbersArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var finalArray = [];
 
 customizePassword();
-
+var passwordlength;
 //validate inputs:
 function customizePassword() {
 
     //create if statements to make sure number (isNaN)
-    var passwordlength = prompt("How many characters?");
+    var howLong = prompt("How many characters?");
     //password too short?     //password too long?
-    if (passwordlength < 8 || passwordlength > 128) {
+    if (howLong < 8 || howLong > 128) {
         alert("Sorry, password should be between 8 and 128 characters. Try again");
         customizePassword();
         // if the user hits Cancel
-    } else if (passwordlength === null) {
+    } else if (howLong === null) {
         alert("Sorry, you have to choose the length. Try again");
         customizePassword();
+        // } else if (howLong)
+    } else {
+        howLong = passwordlength;
     }
     customizeCharacters();
 
@@ -36,15 +42,58 @@ function customizePassword() {
             alert("Sorry, you have to choose at least one option. Try again");
             customizeCharacters();
         }
+        if (hasLower === true) {
+            var addLower = finalArray.concat(lowerArray);
+            finalArray = addLower;
+            console.log(finalArray);
+        }
+        if (hasUpper === true) {
+            var addUpper = finalArray.concat(upperArray);
+            finalArray = addUpper;
+            console.log(finalArray);
+        }
+        if (hasSpecial === true) {
+            var addSpecial = finalArray.concat(specialArray);
+            finalArray = addSpecial;
+            console.log(finalArray);
+        }
+        if (hasNumbers === true) {
+            var addNumbers = finalArray.concat(numbersArray);
+            finalArray = addNumbers;
+            console.log(finalArray);
+        }
     }
+
 
 }
 
+// var passwordLength = howLong.value;
+console.log(passwordlength);
 
+// newPassword = Math.floor(Math.random()* passwordLength)
+
+
+
+
+
+
+
+
+
+var customizeNewButton = document.getElementById("customize-new");
+customizeNewButton.addEventListener("click", function () {
+    customizePassword();
+});
 
 //return;
 
+
+
+
 //create a final Array of Arrays, where random items from selected arrays are stored
+
+
+
+
+
 //display the new password - append to the div
-
-
